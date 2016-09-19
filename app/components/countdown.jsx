cf26ffe0fbe71fdb38c3edd42a83f6b1deff3be9
Vasisht A. Shastry <shastryucsb@gmail.com>
2016-09-19 00:20:06 -0500

    add styles to timer

--- app/components/countdown.jsx
+++ app/components/countdown.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var Clock = require('Clock');
 var CountdownForm = require('CountdownForm');
+var Controls = require('Controls');
 
 var Countdown = React.createClass({
 
@@ -17,9 +18,25 @@ var Countdown = React.createClass({
                 case 'started':
                     this.startTimer();
                     break;
+
+                case 'stopped':
+                    this.setState({
+                        count: 0
+                    });
+
+                case 'paused':
+                    clearInterval(this.timer);
+                    this.timer = undefined;
+                    break;
             }
         }
     },
+    componentWillUnmount: function () {
+        console.log('component did unmount from DOM');
+        clearInterval(this.timer);
+        this.timer = undefined;
+    },
+
     startTimer: function () {
         this.timer = setInterval(() => {
             var newCount = this.state.count - 1;
@@ -28,18 +45,33 @@ var Countdown = React.createClass({
             });
         }, 1000);
     },
+
     handleSetCountdown: function (seconds) {
         this.setState({
             count: seconds,
             countdownStatus: 'started'
         });
     },
+
+    handleStatusChange: function (newStatus) {
+        this.setState({
+            countdownStatus: newStatus
+        });
+    },
+
     render: function () {
-        var {count} = this.state;
+        var {count, countdownStatus} = this.state;
+        var renderControlArea = () => {
+            if (countdownStatus !== 'stopped') {
+                return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>
+            } else {
+                return <CountdownForm onSetCountdown = {this.handleSetCountdown}/>
+            }
+        }
         return (
             <div>
                 <Clock totalSeconds={count}/>
-                <CountdownForm onSetCountdown = {this.handleSetCountdown}/>
+                {renderControlArea() }
             </div>
         )
     }
--- app/tests/components/controls.test.jsx
+++ app/tests/components/controls.test.jsx
@@ -22,8 +22,8 @@ describe('Controls', () => {
         it('should render start when paused', () => {
             var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="paused" />);
             var $el = $(ReactDOM.findDOMNode(controls));
-            var $startButton = $el.find('button:contains(Start)');
-            expect($startButton.length).toBe(1);
+            var $pauseButton = $el.find('button:contains(Start)');
+            expect($pauseButton.length).toBe(1);
         });
 
     });
